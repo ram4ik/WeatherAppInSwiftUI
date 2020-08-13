e598871157a9d97fac180198ba9227d1a301c11f
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-13 10:06:54 +0300

    Initial Commit

---
+++ WeatherAppInSwiftUI/WeatherAppInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppInSwiftUIApp.swift
+//  WeatherAppInSwiftUI
+//
+//  Created by ramil on 13.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
